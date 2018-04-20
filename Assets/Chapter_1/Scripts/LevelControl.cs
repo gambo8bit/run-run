b465d96b35c5303f350a97583f778955f3f2b38b
Gambo8bit <gambo8bit@gmail.com>
2018-04-20 19:24:42 +0900

    04/20

--- Assets/Chapter_1/Scripts/LevelControl.cs
+++ Assets/Chapter_1/Scripts/LevelControl.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class LevelControl : MonoSingleton<LevelControl>
 {
@@ -19,9 +21,12 @@ public class LevelControl : MonoSingleton<LevelControl>
 
     //리스폰위치의 거리(플레이어 기준 얼마나 멀리 둘것이냐)
     private float spawnMargin = 15.0f;
+    public const float SPAWN_MARGIN_MAX = 50.0f;
+    public const float SPWAN_MARGIN_MIN = 20.0f;
+
 
     //그룹안의 도깨비의 수
-    int goalEnemyNum = 1;
+    int enemyNumInGroup = 1;
 
     //Combo 
     int comboCount = 0;
@@ -41,7 +46,11 @@ public class LevelControl : MonoSingleton<LevelControl>
     public eGroupType groupType = eGroupType.NORMAL;
     public eGroupType groupTypeNext = eGroupType.NORMAL;
 
-    
+    //특별패턴
+    eGroupType eventType = eGroupType.NONE;
+    int eventCount = 1;
+    int normalCount = 5;
+    float normalEnemySpeed = EnemyGroup.SPEEDMIN * 5.0f;
 
     
 
@@ -97,7 +106,43 @@ public class LevelControl : MonoSingleton<LevelControl>
                 if (gameManager.enemyGroupCount >= gameManager.enemyGroupCreateMax)
                     break;
 
+                //생성가능상태가 아니면 실행 x
+                if (!bCanCreateEnemy)
+                    break;
 
+                //생성시작지점 플레이어가 아직 지나가지않았다면 실행 x
+                if (player.transform.position.x <= startSpawnLine)
+                    break;
+
+                groupType = groupTypeNext;
+
+                switch (groupType)
+                {
+                    case eGroupType.SLOW:
+                        ProcessGroupSpawn(groupType);
+                        break;
+                    case eGroupType.DECELERATE:
+                        ProcessGroupSpawn(groupType);
+                        break;
+                    case eGroupType.PASSING:
+                        ProcessGroupSpawn(groupType);
+                        break;
+                    case eGroupType.RAPID:
+                        ProcessGroupSpawn(groupType);
+                        break;
+                    case eGroupType.NORMAL:
+                        ProcessGroupSpawn(groupType,normalEnemySpeed);
+                        break;
+                }
+
+                //Enemy 생성 관련 데이터 최신화
+                enemyNumInGroup++; // 다음 생성때 Group안의 Enemy수들을 늘리기위해 Enemy수 데이터++
+                enemyNumInGroup = Mathf.Min(enemyNumInGroup, gameManager.enemyCreateNumMax); //Enemy 수 최대 제한
+                comboCount++; //콤보수 ++
+                gameManager.enemyGroupCount++; //enemyGroup 생성 수
+                bCanCreateEnemy = false; //생성가능 플래그 false
+
+                SelectNextGroup(); //Next Group Type 지정
 
             } while (false);
 
@@ -112,7 +157,91 @@ public class LevelControl : MonoSingleton<LevelControl>
 
     }
 
-    public bool isNormalAI()
+    private void SelectNextGroup()
+    {
+        //보통패턴과 특별패턴 의 전환시점체크
+
+        if (eventType != eGroupType.NONE) //이전의 루프가 특별패턴 이벤트라면 여기 조건문에 들어와 다시 보통패턴 루프돌게 초기화
+        {
+            eventCount--;
+
+            if (eventCount <= 0)
+            {
+                eventType = eGroupType.NONE;
+
+                normalCount = Random.Range(3, 7);
+            }
+
+        }
+        else //eventType이 NONE일때
+        {
+            normalCount--;  // normalCount가 0이 될때 특별패턴EnemyGroup을 다음 GroupType으로 지정
+
+            if(normalCount <= 0)
+            {
+                eventType = (eGroupType)Random.Range(0, 3);
+
+                switch (eventType)
+                {
+                    case eGroupType.SLOW:
+                        eventCount = 1;
+                        break;
+                    case eGroupType.DECELERATE:
+                        break;
+                    case eGroupType.PASSING:
+                        break;
+                    case eGroupType.RAPID:
+                        eventCount = Random.Range(2, 4);
+                        break;
+                }
+            }
+        }
+
+        // 다음 그룹타입을 정하는 데 필요한 데이터 입력(NONE EVENT, EVENT 패턴 2개로 나눠서 입력)
+        if(eventType == eGroupType.NONE)
+        {
+            // 보통 패턴의 EnemyGroup Type
+            float rate;
+
+            rate = (float)comboCount / 10.0f; //콤보수가 높을수록 rate도 높아짐
+
+            rate = Mathf.Clamp01(rate);
+
+            normalEnemySpeed = Mathf.Lerp(EnemyGroup.SPEEDMAX, EnemyGroup.SPEEDMIN, rate); //normal Type 의 속도(콤보시 점점 느려짐)
+
+            nextSpawnLine = Mathf.Lerp(LevelControl.SPAWN_MARGIN_MAX, LevelControl.SPWAN_MARGIN_MIN, rate); //생성되는 간격(콤보시 점점 좁아짐)
+
+            groupTypeNext = eGroupType.NORMAL;
+        }
+        else
+        {
+            groupTypeNext = eventType;
+        }
+
+    }
+
+    private void ProcessGroupSpawn (eGroupType groupType, float speed = 0)
+    {
+        switch (groupType)
+        {
+            case eGroupType.NONE:
+                break;
+            case eGroupType.SLOW:
+                break;
+            case eGroupType.DECELERATE:
+                break;
+            case eGroupType.PASSING:
+                break;
+            case eGroupType.RAPID:
+                break;
+            case eGroupType.NORMAL:
+                break;
+            case eGroupType.NUM:
+                break;
+        }
+    }
+
+        public bool isNormalAI()
     {
         bool bIsNormalType;
 
@@ -159,7 +288,7 @@ public class LevelControl : MonoSingleton<LevelControl>
         //그룹안의 각각의 Enemy들도 생성
         Vector3 baseSpawnPos = spawnPos;
 
-        int wantEnemyNum = goalEnemyNum;
+        int wantEnemyNum = enemyNumInGroup;
 
         //콜리전박스 맨왼쪽 끝을 Enemy생성지점으로 함
         baseSpawnPos.x -= (EnemyGroup.collisionSize * 0.5f - Enemy.collisionSize * 0.5f);
@@ -178,4 +307,5 @@ public class LevelControl : MonoSingleton<LevelControl>
     {
 		
 	}
+
 }
