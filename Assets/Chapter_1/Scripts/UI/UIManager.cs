7ea43562b0e07dd84d1283a7af2c01ac8e001e2d
JoMin <gambo8bit@gmail.com>
2018-05-16 02:07:07 +0900

    최신

---
+++ Assets/Chapter_1/Scripts/UI/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoSingleton<UIManager>
+{
+    public enum eUIType
+    {
+
+    }
+    Dictionary<eUIType,base>
+}
